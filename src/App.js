27b540f122fa1efb4fbd1e727163f8bda2111ce3
Manuella2022 <117217967+Manuella2022@users.noreply.github.com>
2024-02-15 23:57:09 -0500

    #changed background, add an icon, &align it (logo)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import List from './List/List';
+import icon from './icon.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={icon} className="App-icon" alt="icon" />
+
         <List text="This is some text.." />
       </header>
     </div>
