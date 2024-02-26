423a451f8c6cab80f5584e73b7d76c75192880f3
Brett Palmer <brett.palmer@maine.edu>
2024-02-26 10:21:56 -0500

    changing register ui

--- src/Register.js
+++ src/Register.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
-import './App.css'; // Import the shared CSS
+import { Link } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
+import icon from './icon.svg';
 
-function Register() {
+function App() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
@@ -16,8 +19,8 @@ function Register() {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // Add your logic for handling the login (e.g., sending data to a server)
-    console.log('Login submitted with:', { username, password });
+    // Add your logic for handling the Register (e.g., sending data to a server)
+    console.log('Register submitted with:', { username, password });
 
     // Reset the form fields
     setUsername('');
@@ -25,23 +28,30 @@ function Register() {
   };
 
   return (
-    <div>
-      <h2>Login Page</h2>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Username:
-          <input type="text" value={username} onChange={handleUsernameChange} />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input type="password" value={password} onChange={handlePasswordChange} />
-        </label>
-        <br />
-        <button type="submit">Login</button>
-      </form>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={icon} className="App-icon" alt="icon" />
+
+        <div className="Register">
+          <h2>Register Page</h2>
+          <form onSubmit={handleSubmit}>
+            <label>
+              Username:
+              <input type="text" value={username} onChange={handleUsernameChange} />
+            </label>
+            <br />
+            <label>
+              Password:
+              <input type="password" value={password} onChange={handlePasswordChange} />
+            </label>
+            <br />
+            <button type="submit">Register</button>
+          </form>
+        </div>
+      </header>
     </div>
   );
 }
 
-export default Register;
+export default App;
