4d0b598d7934f5d973b8a6f7be28ffe8249788f5
Brett Palmer <brett.palmer@maine.edu>
2024-04-18 13:47:41 -0400

    added a "create a post" button that scrolls to the bottom of the page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,27 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link
+import React, { useRef } from 'react';
+import { Link } from 'react-router-dom'; 
 import logo from './logo.svg';
 import './App.css';
 import Post from './Post/Post';
 
 function App() {
+  const bottomRef = useRef(null); 
+
+  const scrollToBottom = () => {
+    bottomRef.current.scrollIntoView({ behavior: 'smooth' }); 
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <nav>
           <Link to="/profile" className="button-link">Your Profile</Link>
+          <button onClick={scrollToBottom} className="button-link" style={{ marginLeft: '10px' }}>Create a Post</button>
         </nav>
         <Post text="This is some text...2" />
       </header>
+      <div ref={bottomRef} ></div>
     </div>
   );
 }
