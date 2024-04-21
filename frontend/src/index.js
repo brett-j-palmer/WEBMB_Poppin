37a5510e2d319fffc46464ad9367ad329160466a
Brett Palmer <brett.palmer@maine.edu>
2024-04-20 21:10:10 -0400

    can view other user's profile

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,3 @@
-// index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -12,20 +11,22 @@ import reportWebVitals from './reportWebVitals';
 import { UserProvider } from './UserContext'; 
 
 ReactDOM.render(
-  <Router>
-    <UserProvider>
-      <Routes>
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/post" element={<App />} />
-        <Route path="/" element={<Register />} />
-        <Route path="/profile" element={<ProfilePage />} />
-      </Routes>
-    </UserProvider>
-  </Router>,
+  <React.StrictMode>
+    <Router>
+      <UserProvider>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/post" element={<App />} />
+          <Route path="/" element={<Register />} />
+          <Route path="/profile/:username?" element={<ProfilePage />} />
+          <Route path="*" element={<div>Not Found</div>} />  // Catch-all route for undefined paths
+        </Routes>
+      </UserProvider>
+    </Router>
+  </React.StrictMode>,
   document.getElementById('root')
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
